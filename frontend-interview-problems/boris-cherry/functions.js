a59ea2b358d6d829faa89e7d6efb4b21cf7cbae9
Jason Amey <jason.amey@gmail.com>
2023-10-31 14:21:47 -0400

    add reverse, indexOf, isPalindrome, missing, isBalanced and uniq

--- frontend-interview-problems/boris-cherry/functions.js
+++ frontend-interview-problems/boris-cherry/functions.js
@@ -1,5 +1,5 @@
 const functions = {
-  // 1 
+  // 1
   isPrime: (num) => {
     if (num < 2) return false
     if (num === 2) return true
@@ -8,7 +8,7 @@ const functions = {
     }
     return true
   },
-  // 2 
+  // 2
   factorial: (num) => {
     if (num == 0) return 1
     sum = 1
@@ -54,14 +54,104 @@ const functions = {
     }
     return newArr
   },
-  //6 
+  //6
   reduce: (arr, reduceFn, start) => {
     let sum = start
     for (let element of arr) {
       sum = reduceFn(sum, element)
     }
     return sum
+  },
+  //7
+  // O(n) time O(1) space
+  reverse: (str) => {
+    let reversedStr = str
+    let strLen = str.length
+    for (let i = 0; i < Math.floor(strLen / 2); i++) {
+      reversedStr =
+        reversedStr.slice(0, i) +
+        reversedStr.slice(strLen - i - 1, strLen - i) +
+        reversedStr.slice(i + 1, strLen - i - 1) +
+        reversedStr.slice(i, i + 1) +
+        reversedStr.slice(strLen - i, strLen)
+    }
+    return reversedStr
+  },
+  //8
+  indexOf: (arr, target) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        return i
+      }
+    }
+    return -1
+  },
+  //9
+  // O(n) time O(n) space - prob better to do iteratively
+  isPalindrome: function (str) {
+    str = str.toLowerCase().split(' ').join('')
+    if (str.length === 1 || str.length === 0) {
+      return true
+    } else if (str.slice(0, 1) !== str.slice(-1)) {
+      return false
+    } else {
+      return functions.isPalindrome(str.slice(1, -1))
+    }
+  },
+  //10
+  // O(n) time O(1) space
+  missing: function (arr) {
+    if (arr.length === 0) return undefined
+    let max = -Infinity
+    let sum = 0
+    for (const element of arr) {
+      max = element > max ? element : max
+    }
+    if (max === arr.length) return undefined
+    for (let i = 1; i <= max; i++) {
+      sum += i
+    }
+    const sumOfArr = arr.reduce((a, b) => a + b, 0)
+    return sum - sumOfArr
+  },
+  //11
+  // O(n) time O(n) space
+  isBalanced: function (str) {
+    const left = '([{'
+    const right = ')]}'
+    const stack = []
+    for (const char of str) {
+      if (left.indexOf(char) !== -1) {
+        stack.push(char)
+      } else if (right.indexOf(char) !== -1) {
+        if (
+          stack.length == 0 ||
+          left.indexOf(stack.pop()) !== right.indexOf(char)
+        ) {
+          return false
+        }
+      }
+    }
+    return stack.length === 0
+  },
+  //11 
+  //repeat of fibonacci() - memoization included 
+
+  //12 
+  //repeat of isBalanced() - 3 braces already included 
+
+  //13
+  //O(n) time O(n) space
+  uniq: function (arr) {
+    const set = new Set()
+    for (const element of arr) {
+      set.add(element)
+    }
+    return Array.from(set)
   }
 
 }
+
+console.log(functions.uniq([1, 2, 2, 3, 1, 2, 3, 4, 32]))
+
 module.exports = functions
--- frontend-interview-problems/boris-cherry/functions.test.js
+++ frontend-interview-problems/boris-cherry/functions.test.js
@@ -1,6 +1,7 @@
 const functions = require('./functions.js')
 const { primes, factorials, fibonacciNumbers } = require('./utils.js')
 
+//1
 test.skip('determines prime number', () => {
   primes.forEach(item => {
     expect(functions.isPrime(item)).toBe(true)
@@ -19,18 +20,21 @@ test.skip('determines prime number', () => {
   })
 })
 
+//2
 test.skip('determines factorial of a number', () => {
   factorials.forEach((item, idx) => {
     expect(functions.factorial(idx)).toBe(item)
   })
 })
 
+//3
 test.skip('determines the nth fibonacci of a number', () => {
   fibonacciNumbers.forEach((item, idx) => {
     expect(functions.fibonacci(idx)).toBe(item)
   })
 })
 
+//4
 test.skip('checks that array is sorted', () => {
   const { isSorted } = functions
   expect(isSorted([])).toBe(true)
@@ -38,14 +42,70 @@ test.skip('checks that array is sorted', () => {
   expect(isSorted([3, 9, -3, 10])).toBe(false)
 })
 
+//5
 test.skip('checks that array is filtered upon a given condition', () => {
   expect(functions.filter([1, 2, 3, 4], (n) => n < 3)).toEqual([1, 2])
 })
 
-test('checks that an array will reduce upon a given condition', () => {
+//6
+test.skip('checks that an array will reduce upon a given condition', () => {
   const { reduce } = functions
   expect(reduce([1, 2, 3, 4], (a, b) => a + b, 0)).toBe(10)
   expect(reduce([1, 2, 3, 4], (a, b) => a * b, 1)).toBe(24)
   expect(reduce([8, 2, 2], (a, b) => a / b, 32)).toBe(1)
   expect(reduce([], (a, b) => a + b, 0)).toBe(0)
 })
+
+//7
+test.skip('checks that string is reversed', () => {
+  const { reverse } = functions
+  expect(reverse('')).toBe('')
+  expect(reverse('abcdef')).toBe('fedcba')
+  expect(reverse('abc')).toBe('cba')
+  expect(reverse('ab')).toBe('ba')
+  expect(reverse('aab')).toBe('baa')
+})
+
+//8
+test.skip('finds index of target in an array', () => {
+  const { indexOf } = functions
+  expect(indexOf([1, 2, 3], 1)).toBe(0)
+  expect(indexOf([1, 2, 3], 4)).toBe(-1)
+})
+
+//9
+test.skip("checks if string is a palindrome", () => {
+  const { isPalindrome } = functions
+  expect(isPalindrome('')).toBe(true)
+  expect(isPalindrome('abcdcba')).toBe(true)
+  expect(isPalindrome('abcd')).toBe(false)
+  expect(isPalindrome('A man a plan a canal Panama')).toBe(true)
+})
+
+//10
+test.skip("checks for number missing in an unordered array of sequential numbers of 1 to N, where the array length is N - 1", () => {
+  const { missing } = functions
+  expect(missing([])).toBe(undefined)
+  expect(missing([1, 4, 3])).toBe(2)
+  expect(missing([2, 3, 4])).toBe(1)
+  expect(missing([5, 1, 4, 2])).toBe(3)
+  expect(missing([1, 2, 3, 4])).toBe(undefined)
+})
+
+//11
+test.skip("checks higher fibonacci numbers", () => {
+  const { fibonacci: fib2 } = functions
+  expect(fib2(0)).toBe(0)
+  expect(fib2(1)).toBe(1)
+  expect(fib2(10)).toBe(55)
+  expect(fib2(50)).toBe(12586269025)
+})
+
+//12
+test("check for balanced braces for 3 types of braces", () => {
+  const { isBalanced: isBalanced2 } = functions
+  expect(isBalanced2('(foo { bar (baz) [boo] })')).toBe(true)
+  expect(isBalanced2('foo { bar { baz }')).toBe(false)
+  expect(isBalanced2('foo { (bar [baz] } )')).toBe(false)
+})
+
