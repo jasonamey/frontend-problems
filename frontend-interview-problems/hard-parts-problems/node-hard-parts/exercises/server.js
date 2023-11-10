7a2cc13fc4e41170228af09e77b91891aac215bc
Jason Amey <jason.amey@gmail.com>
2023-11-10 09:12:31 -0500

    add node challenges

--- frontend-interview-problems/hard-parts-problems/node-hard-parts/exercises/server.js
+++ frontend-interview-problems/hard-parts-problems/node-hard-parts/exercises/server.js
@@ -9,24 +9,46 @@ function doOnRequest (request, response) {
     // read the index.html file and send it back to the client
     // code here...
 
-    reader.on('data', (chunk) => {
-      htmlPage = chunk.toString()
-      response.end(htmlPage)
-    })
+    const page = fs.readFileSync('index.html', 'utf-8')
+    response.end(page)
+    // reader.on('data', (chunk) => {
+    //   htmlPage = chunk.toString()
+    //   response.end(htmlPage)
+    // })
 
+  } else if (request.method === "GET" && request.url == "/style.css") {
+    const styleSheet = fs.readFileSync("style.css", "utf8");
+    response.end(styleSheet)
   }
+
   else if (request.method === 'POST' && request.url === '/sayHi') {
-    // code here...
+    fs.appendFileSync('hi_log.txt', `${ new Date().toDateString() } somebody said hi!\n`)
+
+    response.end("hi back to you")
 
   }
   else if (request.method === 'POST' && request.url === '/greeting') {
-    // accumulate the request body in a series of chunks
-    // code here...
+    const requestData = []
+    request.on('data', data => {
+      requestData.push(data.toString())
+    }).on('end', () => {
+      const [requestText] = requestData
+      fs.appendFileSync('hi_log.txt', `${ new Date().toDateString() } REQUEST TEXT: ${ requestText }\n`)
+
+      if (requestText === 'hello') {
+        response.end('hello there!')
+      }
+
+      else if (requestText === 'what\'s up') {
+        response.end('the sky!')
+      } else {
+        response.end('good morning!')
+      }
+    })
 
   }
   else {
-    // Handle 404 error: page not found
-    // code here...
+    response.end("404 ERROR - oh no !")
 
   }
 }
